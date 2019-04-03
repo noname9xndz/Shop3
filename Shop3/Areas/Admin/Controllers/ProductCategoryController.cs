0b097f2a6b07195687497f02af8b74f5d0e1dc59
noname9xndz <44211401+noname9xndz@users.noreply.github.com>
2019-04-03 23:34:47 +0700

    sử dụng easyui hiển thị tree productcategory

---
+++ Shop3/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shop3.Application.Interfaces;
+
+namespace Shop3.Areas.Admin.Controllers
+{
+    public class ProductCategoryController : BaseController
+    {
+        IProductCategoryService _productCategoryService;
+        public ProductCategoryController(IProductService productService, IProductCategoryService productCategoryService)
+        {
+            _productCategoryService = productCategoryService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var model = _productCategoryService.GetAll();
+            return new OkObjectResult(model);
+        }
+
+
+
+        #endregion
+    }
+}
---
+++ Shop3/wwwroot/app/controllers/product-category/index.js
@@ -0,0 +1,40 @@
+﻿
+
+var productCategoryController = function () {
+
+    this.initialize = function () {
+        loadData();
+    }
+    // load dữ liệu productcategory dạng tree  https://www.jeasyui.com/
+    function loadData() {
+        $.ajax({
+            url: '/Admin/ProductCategory/GetAll',
+            dataType: 'json',
+            success: function (response) {
+
+                var data = [];
+                $.each(response, function (i, item) {
+                    data.push({
+                        id: item.Id,
+                        text: item.Name,
+                        parentId: item.ParentId,
+                        sortOrder: item.SortOrder
+                    });
+                     
+                });
+
+                var treeArr = common.unflattern(data); // tạo tree
+                treeArr.sort(function (a, b) {
+                    return a.sortOrder - b.sortOrder;
+                });
+                //var $tree = $('#treeProductCategory');
+                // sử dụng easyui hiển thị tree
+                $('#treeProductCategory').tree({
+                    data: treeArr,
+                    dnd: true
+                });
+
+            }
+        });
+    }
+}
