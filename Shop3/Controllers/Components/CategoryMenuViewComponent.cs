35dc8a047fb22f285c7ac607617b656a4832fde2
noname9xndz <44211401+noname9xndz@users.noreply.github.com>
2019-04-22 12:57:37 +0700

    Tạo  ViewComponent dùng chung trên view client

---
+++ Shop3/Controllers/Components/CategoryMenuViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop3.Controllers.Components
+{
+    public class CategoryMenuViewComponent : ViewComponent
+    {
+        // https://docs.microsoft.com/en-us/aspnet/core/mvc/views/view-components?view=aspnetcore-2.2
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
