9cfd60f83aa4cca69c24459636bd08014f7b2dc3
noname9xnd <noname9xnd@gmail.com>
2019-09-06 18:45:52 +0700

    upup

--- Shop3.DependencyResolver/DependencyRegister.cs
+++ Shop3.DependencyResolver/DependencyRegister.cs
@@ -1,10 +1,59 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Shop3.DependencyResolver
 {
-    interface IDependencyResolver
+    public class DependencyRegister : IDependencyRegister
     {
+        private readonly IServiceCollection serviceCollection;
+
+        public DependencyRegister(IServiceCollection serviceCollection)
+        {
+            this.serviceCollection = serviceCollection;
+        }
+
+        void IDependencyRegister.AddScoped<TService>()
+        {
+            serviceCollection.AddScoped<TService>();
+        }
+
+        void IDependencyRegister.AddScoped<TService, TImplementation>()
+        {
+            serviceCollection.AddScoped<TService, TImplementation>();
+        }
+
+        void IDependencyRegister.AddScopedForMultiImplementation<TService, TImplementation>()
+        {
+            serviceCollection.AddScoped<TImplementation>()
+                .AddScoped<TService, TImplementation>(s => s.GetService<TImplementation>());
+        }
+
+        void IDependencyRegister.AddSingleton<TService>()
+        {
+            serviceCollection.AddSingleton<TService>();
+        }
+
+        void IDependencyRegister.AddSingleton<TService, TImplementation>()
+        {
+            serviceCollection.AddSingleton<TService, TImplementation>();
+        }
+
+        void IDependencyRegister.AddTransient<TService>()
+        {
+            serviceCollection.AddTransient<TService>();
+        }
+
+        void IDependencyRegister.AddTransient<TService, TImplementation>()
+        {
+            serviceCollection.AddTransient<TService, TImplementation>();
+        }
+
+        void IDependencyRegister.AddTransientForMultiImplementation<TService, TImplementation>()
+        {
+            serviceCollection.AddTransient<TImplementation>()
+                .AddTransient<TService, TImplementation>(s => s.GetService<TImplementation>());
+        }
     }
 }
